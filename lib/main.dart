448ad89f97fb909b21043c6abd529427c875a43b
Leroi <67743669+LeroiDev@users.noreply.github.com>
2025-03-27 13:30:40 +0200

    basic-state-management

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,59 @@
+import 'dart:math';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int count = 0;
+  // Color blueBar = Colors.lightBlueAccent;
+  // Color blueBig = const Color.fromARGB(255, 154, 253, 219);
+  // Color temp = Colors.black;
+  int appbarCount=0;
+  int scaffoldCount=0;
+
+  List<Color> colorsList = [Colors.redAccent,Colors.cyanAccent,Colors.limeAccent,Colors.amberAccent,Colors.pinkAccent,Colors.orangeAccent];
+  Random ranColor1 = new Random();
+
+  String kickSound = "lib/assets/audio/Kick.wav";
+  final audioPlayer = AudioPlayer();
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 154, 253, 219),
+        backgroundColor: colorsList[scaffoldCount],
         appBar: AppBar(
-          backgroundColor: Colors.lightBlueAccent,
+          backgroundColor: colorsList[appbarCount],
           centerTitle: true,
-          title: const Text('My App'),
+          title: Text('$count'),
         ),
         body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.pinkAccent,
+              MaterialButton(
                 child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.brown,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.greenAccent,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blueGrey,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.pinkAccent,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.brown,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.greenAccent,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blueGrey,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.pinkAccent,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.brown,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.greenAccent,
-                child: Image.asset("lib/assets/images/myDash.png"),
-              ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blueGrey,
-                child: Image.asset("lib/assets/images/myDash.png"),
+                onPressed: () => {
+                  setState(() {
+                    count++;
+                    appbarCount = ranColor1.nextInt(6);
+                    scaffoldCount = ranColor1.nextInt(6);
+                    audioPlayer.play(AssetSource(kickSound));
+                  }),
+                },
               ),
             ],
           ),
